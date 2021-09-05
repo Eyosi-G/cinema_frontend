8387c5cf783261d1c7bfd56cdfe2e0bbfc5983d6
Eyosiyas Girma <eyosiyasgirma0@gmail.com>
2021-09-05 20:36:41 +0300

    production url changed

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
-const prodURL = 'https://cinema-addis.herokuapp.com/api/v1';
+const prodURL = 'https://eyosi-cinema-addis.herokuapp.com/api/v1';
 const devURL = 'http://localhost:8080/api/v1'
 const config = {
   baseURL: prodURL,
